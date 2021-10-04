50bd42008e0836164dce4197ec8b18a958cccd02
POMXARK <1997pom@gmail.com>
2021-10-04 12:30:40 +1000

    1C_TG_BOT pyTelegramBotAPI + Timezone + none_stop_ + keyboard + test

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from expiration_date import expiration_date
 
 ### Deploy HEROKU ###
 """
+heroku login
 git init
 git push heroku master
 """
@@ -25,6 +26,77 @@ keyboard = types.ReplyKeyboardMarkup() # обновить клавиатуру
 #markup = types.ReplyKeyboardMarkup(resize_keyboard=False)
 #item1 = types.KeyboardButton("Дата окончания срока годности")
 #markup.add(item1)
+
+######
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    #sti = open('static/welcome.webp', 'rb')
+   # bot.send_sticker(message.chat.id, sti)
+
+    # keyboard
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=False)
+    item1 = types.KeyboardButton("🎲 Рандомное число")
+    item2 = types.KeyboardButton("😊 Как дела?")
+    item3 = types.KeyboardButton("Дата окончания срока годности")
+    markup.add(item1, item2,item3)
+
+    bot.send_message(message.chat.id,
+                     "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный чтобы быть подопытным кроликом.".format(
+                         message.from_user, bot.get_me()),
+                     parse_mode='html', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def lalala(message):
+    if message.chat.type == 'private':
+        if message.text == '🎲 Рандомное число':
+            bot.send_message(message.chat.id, str(random.randint(0, 100)))
+        elif message.text == 'Дата окончания срока годности':
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            item1 = types.InlineKeyboardButton("24", callback_data='24')
+            item2 = types.InlineKeyboardButton("36", callback_data='36')
+            markup.add(item1, item2)
+            bot.send_message(message.chat.id, 'Выбери количество часов', reply_markup=markup)
+        elif message.text == '😊 Как дела?':
+
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            item1 = types.InlineKeyboardButton("Хорошо", callback_data='good')
+            item2 = types.InlineKeyboardButton("Не очень", callback_data='bad')
+
+            markup.add(item1, item2)
+
+            bot.send_message(message.chat.id, 'Отлично, сам как?', reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, 'Я не знаю что ответить  😢')
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == 'good':
+                bot.send_message(call.message.chat.id, 'Вот и отличненько 😊')
+            elif call.data == 'bad':
+                bot.send_message(call.message.chat.id, 'Бывает 😢')
+            #if call.data == '24':
+            #    pass
+            # remove inline buttons
+            #bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="😊 Как дела?",
+            #                      reply_markup=None)
+
+            # show alert
+            #bot.answer_callback_query(callback_query_id=call.id, show_alert=False,
+            #                          text="ЭТО ТЕСТОВОЕ УВЕДОМЛЕНИЕ!!11")
+
+    except Exception as e:
+        print(repr(e))
+
+
+
+
+
+####
+
 
 @bot.message_handler(content_types=['text'])
 def welcome(pm):
